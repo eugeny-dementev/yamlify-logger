cb359fe42e19bade2c9720ced75dfd5caa661e42
Eugeny Dementev <eugeny.dementev@ya.ru>
2024-03-09 18:44:24 +0800

    add line default template

---
+++ src/line.js
@@ -0,0 +1,13 @@
+exports.line = function lineDefaultTemplate(props) {
+  const parts = [
+    props.timestamp ? `[${props.timestamp}]` : '',
+    props.label ? `[${props.label}]` : '',
+    props.level ? `[${props.level}]` : '',
+    props.from ? `[${props.from}]` : '',
+    props.message ? ` - ${props.message}` : '',
+  ]
+
+  return parts
+    .filter((value) => value)
+    .join('');
+}
